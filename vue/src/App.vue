568eb89263e96688bc66d48aba7aac62066cd23c
cybeq <dzordzo221@gmail.com>
2023-03-30 10:20:53 +0200

    Vue init

---
+++ vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+body{
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ vue/src/components/ListComponent.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ListController",
+  data(){
+    return{
+      list:undefined,
+      clients:undefined,
+      orders:undefined,
+      cars:undefined,
+      employees:undefined
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
